88a25d536769557d63da6cfd6f0810020a30f0f5
San4hi <antonsayfermay@gmail.com>
2023-08-09 12:17:10 +1000

    add class properties

--- src/objects.cpp
+++ src/objects.cpp
@@ -1,17 +1,28 @@
 #include "../headers/headers.hpp"
 
 class Property {
-    
+private:
+    string name;
+public:
+    //Get and set name property
+    string GetName() { return name; }
+    void SetName(string new_name) { name = new_name; }
+
+    Property(string name) {
+        SetName(name);
+    }
 };
 
 class Object {
 private:
     string name;
-
+    vector<Property> properties;
 public:
     //Get and set for name object
     string GetName() { return name; }
     void SetName(string new_name) { name = new_name; }
+
+    //Properties func
     
     //Object constructors
     Object(string name) {
