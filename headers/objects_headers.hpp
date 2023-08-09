ae5401d02ff518fd0e5a0f21afea0b5e8042861e
San4hi <antonsayfermay@gmail.com>
2023-08-09 12:36:21 +1000

    add properties - transform header

--- headers/objects_headers.hpp
+++ headers/objects_headers.hpp
@@ -1,5 +1,29 @@
 #include "../headers/headers.hpp"
 
+class Property {
+private:
+    string name;
+public:
+    string GetName();
+    void SetName(string new_name);
+
+    Property(string name);
+};
+
+class Property_Transform : Property {
+private:
+    Vector2<float> position;
+public:
+    //Get and set position
+    Vector2<float> GetPosition();
+
+    Vector2<float> SetPosition(Vector2<float> new_pos);
+
+    Vector2<float> SetPosition(float new_x, float new_y);
+    
+    Property_Transform();
+};
+
 class Object {
 private:
     string name;
