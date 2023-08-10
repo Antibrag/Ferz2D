00e4ad316ebba848ebd1399737881e3ed3ce0d7f
San4hi <antonsayfermay@gmail.com>
2023-08-10 08:34:14 +0300

    add AddProperty() and EraseProperty() func class Object

--- src/objects.cpp
+++ src/objects.cpp
@@ -8,19 +8,21 @@ public:
     string GetName() { return name; }
     void SetName(string new_name) { name = new_name; }
 
+    Property();
+
     Property(string name) {
         SetName(name);
     }
 };
 
-class Property_Transform : Property {
+class Property_Transform : public Property {
 private:
     Vector2<float> position;
 public:
     //Get and set position
     Vector2<float> GetPosition() { return position; }
 
-    Vector2<float> SetPosition(Vector2<float> new_pos) { position = new_position; }
+    Vector2<float> SetPosition(Vector2<float> new_position) { position = new_position; }
 
     Vector2<float> SetPosition(float new_x, float new_y) {
         position.x = new_x;
@@ -28,9 +30,10 @@ public:
     }
 
     Property_Transform() {
-        position = 0;
+        position.x = 0;
+        position.y = 0;
     }
-}
+};
 
 
 class Object {
@@ -40,9 +43,18 @@ private:
 public:
     //Get and set for name object
     string GetName() { return name; }
+
     void SetName(string new_name) { name = new_name; }
 
     //Properties func
+    void AddProperty(Property& new_property) { properties.push_back(new_property); }
+
+    void EraseProperty(string name_property) {
+        for (auto i { properties.begin() }; i != properties.cend(); i++) {
+            if ((*i).GetName() == name_property)
+                properties.erase(i);
+        }
+    }
     
     //Object constructors
     Object(string name) {
